18d532283d3dfa5fe9e16ece24967f4ea4228803
Kedarnag Mukanahallipatna <ksatish@qwinixtech.com>
2015-07-23 17:56:26 +0530

    [development setting_up_application] Initialized the Application.

--- main.go
+++ main.go
@@ -1,1 +1,18 @@
-main.go
+package main
+
+import (
+	"fmt"
+	"github.com/gorilla/mux"
+	"log"
+	"net/http"
+)
+
+func main() {
+	r := mux.NewRouter()
+	fmt.Println("Welcome to Android-Go")
+	http.Handle("/", r)
+
+	// HTTP Listening Port
+	log.Println("main : Started : Listening on: http://localhost:3010 ...")
+	log.Fatal(http.ListenAndServe("0.0.0.0:3010", nil))
+}
