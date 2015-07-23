0d813f1196e313fd01577148d07bdd72c3f22574
Kedarnag Mukanahallipatna <ksatish@qwinixtech.com>
2015-07-23 22:04:29 +0530

    [development sign_up] Started with the Sign_Up API.

--- api/v1/controllers/account/sign_up.go
+++ api/v1/controllers/account/sign_up.go
@@ -1,1 +1,29 @@
 package account
+
+import (
+	"fmt"
+)
+
+type registrationController struct{}
+
+var Registration registrationController
+
+func (r registrationController) Create(rw http.ResponseWriter, req *http.Request) {
+
+	body, err := ioutil.ReadAll(req.Body)
+	var u models.User
+
+	if err != nil {
+		panic(err)
+	}
+	err = json.Unmarshal(body, &u)
+	if err != nil {
+		panic(err)
+	}
+
+	db, err := sql.Open("postgres", "password=password host=localhost dbname=android_go sslmode=disable")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+}
