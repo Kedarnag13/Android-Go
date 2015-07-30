0d65b9f29a5c7852d50f87ad2c6f35e1b8226357
Kedarnag Mukanahallipatna <ksatish@qwinixtech.com>
2015-07-30 12:00:05 +0530

    [development sms_verification] Adds the API to send SMS invite.

---
+++ api/v1/controllers/users/sms_invite.go
@@ -0,0 +1,21 @@
+package users
+
+import (
+	"github.com/sfreiberg/gotwilio"
+	"net/http"
+)
+
+type messageController struct{}
+
+var Message messageController
+
+func (m messageController) Send(rw http.ResponseWriter, req *http.Request) {
+	accountSid := "AC62e6cfa0a301115e75e91cd6fb176e5f"
+	authToken := "d25a1dd728cec4edc7d1b1e34d9b04b6"
+	twilio := gotwilio.NewTwilioClient(accountSid, authToken)
+
+	from := "+14805683241"
+	to := "+917022665448"
+	message := "Welcome to Android-Go."
+	twilio.SendSMS(from, to, message, "", "")
+}
